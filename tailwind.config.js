fb682e59590e310b836cc630a2dd12a4e3976762
nicholas harper <60901537+NicholasHarper0813@users.noreply.github.com>
2024-12-06 07:11:58 -0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js,svelte,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#A8EAFF",
+        dark1: "#191817",
+        dark2: "#312923",
+      },
+      fontSize: {
+        "heading-large": "130px",
+      },
+      fontFamily: {
+        inter: ["Inter Tight", "sans-serif"],
+        display: ["Inter Display", "sans-serif"],
+        instrument: ["Instrument Serif", "sans-serif"],
+        victor: ["Victor Serif", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
